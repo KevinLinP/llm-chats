1c2faa91119100458d6f2cf092050f386b055787
Kevin Lin <kevin.lin.p@gmail.com>
2024-01-08 00:59:32 -0800

    add streaming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,26 +11,59 @@
     setupCrypto();
   }
 
+  // TODO: make model selectable
+
   const openai = new OpenAI({
     apiKey: 'not needed',
     baseURL: 'http://localhost:1234/v1/',
     dangerouslyAllowBrowser: true
   });
 
-  let userMessage = '';
+  let temperature = 0.7;
+  let systemMessage = 'You are a helpful assistant.';
+  let userMessage = 'Write a haiku about the sunset.';
+  // TODO: render Markdown properly
   let assistantMessage = '';
 
-  const handleHello = async () => {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: userMessage }]
+  const handleSend = async () => {
+    assistantMessage = '';
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: systemMessage},
+        { role: 'user', content: userMessage }
+      ],
+      temperature,
+      stream: true
     });
-    assistantMessage = chatCompletion.choices[0].message.content;
+
+    for await (const chunk of completion) {
+      const choice = chunk.choices[0];
+      const chunkContent = choice.delta.content;
+      if (chunkContent) {
+        assistantMessage += chunkContent;
+      }
+      if (choice.finish_reason && choice.finish_reason != 'stop') {
+        assistantMessage += choice.finish_reason;
+      }
+    }
   }
 
+  // TODO: turn into array of messages
+  // TODO: better layout
 </script>
 
-<textarea bind:value={userMessage} rows="5"/>
+<div class="mb-3">
+  <label for="system-message" class="form-label">System Message</label>
+  <textarea id="system-message" bind:value={systemMessage} class="form-control"  rows="5"/>
+</div>
+
+<label for="user-message" class="form-label">User Message</label>
+<textarea id="user-message" bind:value={userMessage} class="form-control" rows="5"/>
+
+<!-- <label for="temperature" class="form-label">Temperature</label>
+<input id="temperature" type="number" min="0" max="2" bind:value={temperature} class="form-control"/> -->
 
 <p>{assistantMessage}</p>
 
-<button class="btn btn-primary" on:click={handleHello}>Hello!</button>
+<button class="btn btn-primary" on:click={handleSend}>Send</button>
