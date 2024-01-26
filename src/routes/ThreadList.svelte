b1818cf3acca8949e0cf7cdd58456331eb88e140
Kevin Lin <kevin.lin.p@gmail.com>
2024-01-26 00:45:40 -0800

    starting encryption

--- src/routes/ThreadList.svelte
+++ src/routes/ThreadList.svelte
@@ -1,6 +1,6 @@
 <script>
-  import { onDestroy } from 'svelte';
-  import { collection, query, orderBy, onSnapshot, addDoc, serverTimestamp, deleteDoc } from "firebase/firestore";
+  import { getContext, onDestroy } from 'svelte';
+  import { collection, query, orderBy, onSnapshot, addDoc, serverTimestamp, deleteDoc, Bytes } from "firebase/firestore";
   import { writable } from 'svelte/store';
 
   export let db;
@@ -14,9 +14,25 @@
   });
   onDestroy(unsubscribe);
 
+  const encryptionKey = getContext('encryptionKey');
+
   const handleCreateThread = async () => {
+    const enc = new TextEncoder();
+    const plain = {title: 'testing'}
+    const encoded = enc.encode(JSON.stringify(plain));
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    const encrypted = await window.crypto.subtle.encrypt(
+      {
+        name: 'AES-GCM',
+        iv,
+      },
+      encryptionKey,
+      encoded,
+    );
+
     const docRef = await addDoc(collection(db, "threads"), {
-      plain: {},
+      iv: Bytes.fromUint8Array(iv),
+      encrypted: Bytes.fromUint8Array(new Uint8Array(encrypted)),
       created: serverTimestamp(),
       updated: serverTimestamp(),
     });
@@ -26,12 +42,31 @@
   const handleDestroy = (thread) => {
     deleteDoc(thread.ref);
   }
+
+  const plain = (thread) => {
+    const iv = new Uint8Array(thread.data().iv.toUint8Array());
+    const encrypted = new Uint8Array(thread.data().encrypted.toUint8Array());
+
+    const decrypted = window.crypto.subtle.decrypt(
+      {
+        name: 'AES-GCM',
+        iv,
+      },
+      encryptionKey,
+      encrypted,
+    );
+
+    return decrypted.then((decrypted) => {
+      const dec = new TextDecoder();
+      return JSON.parse(dec.decode(decrypted));
+    });
+  }
 </script>
 
 {#each $threads as thread (thread.id)}
   <div class="mb-2">
     <button class="btn btn-primary" on:click={setCurrentThreadRef(thread.ref)}>
-      {thread.data().plain?.title || 'Untitled'}
+      {plain(thread).title || 'Untitled'}
     </button>
     <button class="btn btn-danger" on:click={handleDestroy(thread)}>
       🗑
