5265559ee4a19b0791984364a804b00ed8dde7c8
Kevin Lin <kevin.lin.p@gmail.com>
2024-02-05 23:02:03 -0800

    Thread: decrypt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,48 @@
 <script>
 	import { getFirestore, onSnapshot } from 'firebase/firestore';
-  import { getContext, onDestroy } from 'svelte';
+	import { getContext, onDestroy } from 'svelte';
 
-  import Thread from './Thread.svelte';
-  import ThreadList from './ThreadList.svelte';
+	import Thread from './Thread.svelte';
+	import ThreadList from './ThreadList.svelte';
 	import { writable } from 'svelte/store';
 
 	const db = getFirestore();
 
-  let unsubscribe = null;
-  let currentThreadRef = null;
-  const currentThread = writable(null);
+	let unsubscribe = null;
+	let currentThreadRef = null;
+	const currentThread = writable(null);
 
-  const encryptionKey = getContext('encryptionKey')
-  console.log(encryptionKey);
+	const setCurrentThreadRef = (threadRef) => {
+		currentThreadRef = threadRef;
+	};
 
-  const setCurrentThreadRef = (threadRef) => {
-    currentThreadRef = threadRef;
-  }
+	$: {
+		if (currentThreadRef) {
+			if (unsubscribe) unsubscribe();
 
-  $: {
-    if (currentThreadRef) {
-      if (unsubscribe) unsubscribe();
+			unsubscribe = onSnapshot(currentThreadRef, (doc) => {
+				currentThread.set(doc);
+			});
+		}
+	}
 
-      unsubscribe = onSnapshot(currentThreadRef, (doc) => {
-        currentThread.set(doc);
-      });
-    }
-  }
-
-  onDestroy(() => {
-    if (unsubscribe) unsubscribe();
-  });
+	onDestroy(() => {
+		if (unsubscribe) unsubscribe();
+	});
 </script>
 
 <div class="d-flex flex-direction-row" style="gap: 3rem;">
-  <div>
-    <ThreadList {db} {setCurrentThreadRef}/>
-  </div>
+	<div>
+		<ThreadList {db} {setCurrentThreadRef} />
+	</div>
 
-  <div class="flex-grow-1">
-    {#if $currentThread}
-      {#each [$currentThread] as thread (thread.id)}
-        {#if thread}
-          <Thread {db} thread={thread}/>
-        {/if}
-      {/each}
-    {/if}
-  </div>
+	<div class="flex-grow-1">
+		{#if $currentThread}
+			{#each [$currentThread] as thread (thread.id)}
+				{#if thread}
+					<Thread {db} {thread} />
+				{/if}
+			{/each}
+		{/if}
+	</div>
 </div>
--- src/routes/Thread.svelte
+++ src/routes/Thread.svelte
@@ -1,149 +1,189 @@
 <script>
+	import { getContext } from 'svelte';
 	import { serverTimestamp, updateDoc } from 'firebase/firestore';
-  import OpenAI from 'openai';
+	import OpenAI from 'openai';
 
-  export let thread;
-  export let db;
+	export let thread;
 
-  $: {
-    console.log(thread);
-    console.log(thread.data());
-  }
+	const encryptionKey = getContext('encryptionKey');
+	console.log(encryptionKey);
 
-  const openai = new OpenAI({
-    apiKey: 'not needed',
-    baseURL: 'http://localhost:1234/v1/',
-    dangerouslyAllowBrowser: true
-  });
+	$: {
+		console.log(thread.data());
+	}
 
-  let systemMessage = 'You are a helpful assistant.';
-  let userMessage = '';
-  // TODO: render Markdown properly
-  let assistantMessage = '';
-  let error = '';
-  let title = thread.data().plain?.title || '';
-  
-  $: plain = thread.data()?.plain;
-  $: messages = plain.messages;
-  $: console.log({plain});
+	const openai = new OpenAI({
+		apiKey: 'not needed',
+		baseURL: 'http://localhost:1234/v1/',
+		dangerouslyAllowBrowser: true
+	});
 
-  const handleSend = async () => {
-    assistantMessage = '';
-    error = '';
+	let systemMessage = 'You are a helpful assistant.';
+	let userMessage = '';
+	// TODO: render Markdown properly
+	let assistantMessage = '';
+	let error = '';
+	let title = thread.data().plain?.title || '';
 
-    const newMessages = [
-      ...(messages || [{role: 'system', content: systemMessage}]),
-      { role: 'user', content: userMessage }
-    ];
+	$: iv = new Uint8Array(thread.data().iv.toUint8Array());
+	$: encrypted = new Uint8Array(thread.data().encrypted.toUint8Array());
 
-    try {
-      const completion = await openai.chat.completions.create({
-        messages: newMessages, 
-        stream: true
-      });
+	let plain = null;
 
-      for await (const chunk of completion) {
-        const choice = chunk.choices[0];
-        const chunkContent = choice.delta.content;
-        if (chunkContent) {
-          assistantMessage += chunkContent;
-        }
-        if (choice.finish_reason && choice.finish_reason != 'stop') {
-          assistantMessage += choice.finish_reason;
-        }
-      }
+	$: {
+		const something = async () => {
+			const decrypted = await window.crypto.subtle.decrypt(
+				{
+					name: 'AES-GCM',
+					iv
+				},
+				encryptionKey,
+				encrypted
+			);
 
-      newMessages.push({ 
-        role: 'assistant',
-        content: assistantMessage.trim()
-      });
+			plain = JSON.parse(new TextDecoder().decode(decrypted));
+		};
 
-      updateDoc(thread.ref, {
-        plain: {
-          ...plain,
-          messages: newMessages
-        },
-        updated: serverTimestamp()
-      });
+		something();
+	}
 
-      userMessage = '';
-      assistantMessage = '';
-    } catch (e) {
-      error = e;
-    }
-  }
+	$: console.log({ plain });
 
-  const saveTitle = async () => {
-    updateDoc(thread.ref, {
-      plain: {
-        ...plain,
-        title
-      },
-      updated: serverTimestamp()
-    });
-  }
+	$: messages = plain?.messages || [];
+
+	const handleSend = async () => {
+		assistantMessage = '';
+		error = '';
+
+		const newMessages = [
+			...(messages || [{ role: 'system', content: systemMessage }]),
+			{ role: 'user', content: userMessage }
+		];
+
+		try {
+			const completion = await openai.chat.completions.create({
+				messages: newMessages,
+				stream: true
+			});
+
+			for await (const chunk of completion) {
+				const choice = chunk.choices[0];
+				const chunkContent = choice.delta.content;
+				if (chunkContent) {
+					assistantMessage += chunkContent;
+				}
+				if (choice.finish_reason && choice.finish_reason != 'stop') {
+					assistantMessage += choice.finish_reason;
+				}
+			}
+
+			newMessages.push({
+				role: 'assistant',
+				content: assistantMessage.trim()
+			});
+
+			updateDoc(thread.ref, {
+				plain: {
+					...plain,
+					messages: newMessages
+				},
+				updated: serverTimestamp()
+			});
+
+			userMessage = '';
+			assistantMessage = '';
+		} catch (e) {
+			error = e;
+		}
+	};
+
+	const saveTitle = async () => {
+		updateDoc(thread.ref, {
+			plain: {
+				...plain,
+				title
+			},
+			updated: serverTimestamp()
+		});
+	};
 </script>
 
 <div class="mb-3">
-  <input type="text" bind:value={title} on:blur={saveTitle} class="form-control title-input" class:blank={!title.length} placeholder="title"/>
+	<input
+		type="text"
+		bind:value={title}
+		on:blur={saveTitle}
+		class="form-control title-input"
+		class:blank={!title.length}
+		placeholder="title"
+	/>
 </div>
 
-
 {#if messages?.length > 0}
-  {#each messages as message, i (i)}
-    <div class="mb-3">
-      <div>{message.role}</div>
-      <div>{message.content}</div>
-    </div>
-  {/each}
+	{#each messages as message, i (i)}
+		<div class="mb-3">
+			<div>{message.role}</div>
+			<div>{message.content}</div>
+		</div>
+	{/each}
 {:else}
-  <div class="mb-3">
-    <label for="system-message" class="form-label minimal-input">system</label>
-    <textarea id="system-message" bind:value={systemMessage} class="form-control minimal-input" rows="1"/>
-  </div>
+	<div class="mb-3">
+		<label for="system-message" class="form-label minimal-input">system</label>
+		<textarea
+			id="system-message"
+			bind:value={systemMessage}
+			class="form-control minimal-input"
+			rows="1"
+		/>
+	</div>
 {/if}
 
 <div class="d-flex flex-direction-row align-items-end">
-  <div class="flex-grow-1">
-    <label for="user-message" class="form-label">user</label>
-    <textarea id="user-message" bind:value={userMessage} on:keydown={(e) => {
-      if (e.key === 'Enter' && !e.shiftKey && !e.ctrlKey) {
-        e.preventDefault();
-        handleSend();
-      }
-    }} class="form-control minimal-input"  rows="1"/>
-  </div>
+	<div class="flex-grow-1">
+		<label for="user-message" class="form-label">user</label>
+		<textarea
+			id="user-message"
+			bind:value={userMessage}
+			on:keydown={(e) => {
+				if (e.key === 'Enter' && !e.shiftKey && !e.ctrlKey) {
+					e.preventDefault();
+					handleSend();
+				}
+			}}
+			class="form-control minimal-input"
+			rows="1"
+		/>
+	</div>
 
-  <div>
-    <button class="btn btn-text" on:click={handleSend}>Send</button>
-  </div>
+	<div>
+		<button class="btn btn-text" on:click={handleSend}>Send</button>
+	</div>
 </div>
-
 
 <p>{assistantMessage}</p>
 <p class="text-danger">{error}</p>
 
-
 <style lang="scss">
-  .title-input {
-    border-radius: 0;
-    &:not(:focus) {
-      &:not(.blank) {
-        border: none;
-        padding-left: 0;
-      }
-    }
+	.title-input {
+		border-radius: 0;
+		&:not(:focus) {
+			&:not(.blank) {
+				border: none;
+				padding-left: 0;
+			}
+		}
 
-    &:focus, &.blank {
-      border-top: none;
-      border-left: none;
-      border-right: none;
-    }
-  }
-  .minimal-input {
-    border-top: none;
-    border-left: none;
-    border-right: none;
-    border-radius: 0;
-  }
+		&:focus,
+		&.blank {
+			border-top: none;
+			border-left: none;
+			border-right: none;
+		}
+	}
+	.minimal-input {
+		border-top: none;
+		border-left: none;
+		border-right: none;
+		border-radius: 0;
+	}
 </style>
