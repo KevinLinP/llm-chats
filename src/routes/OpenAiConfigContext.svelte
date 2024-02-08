c49736b391e0f9208dd535ef3ec46c6cf978ad5c
Kevin Lin <kevin.lin.p@gmail.com>
2024-02-07 22:26:28 -0800

    OpenAiConfigContext: fix context setting

--- src/routes/OpenAiConfigContext.svelte
+++ src/routes/OpenAiConfigContext.svelte
@@ -6,12 +6,12 @@
 	const openAiConfig = writable(null);
 	setContext('openAiConfig', openAiConfig);
 
-	const readKey = async () => {
-		const auth = getContext('auth');
-		const db = getContext('db');
-		const userRef = doc(db, 'users', auth.currentUser.uid);
-		setContext('userRef', userRef);
+	const auth = getContext('auth');
+	const db = getContext('db');
+	const userRef = doc(db, 'users', auth.currentUser.uid);
+	setContext('userRef', userRef);
 
+	const readKey = async () => {
 		let user = await getDoc(userRef);
 		if (!user.exists()) return;
 
