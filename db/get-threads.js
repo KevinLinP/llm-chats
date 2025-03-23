0b0050ad838a3842645a58984206ef5d34dd07b8
Kevin Lin <kevin.lin.p@gmail.com>
2025-03-23 12:08:54 -0700

    migrate timestamps

--- db/get-threads.js
+++ db/get-threads.js
@@ -1,16 +1,6 @@
-import fs from 'fs';
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-
-const serviceAccount = JSON.parse(fs.readFileSync('db/firebase-adminsdk.secret.json'));
+import { db } from './lib/db.js';
 
 const LIMIT = 10;
-
-initializeApp({
-  credential: cert(serviceAccount)
-});
-
-const db = getFirestore();
 
 const snapshot = await db.collection('threads').orderBy('created', 'desc').limit(LIMIT).get();
 
