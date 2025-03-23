2b0795706fc6595104169fe80e97f10b06b4d17d
Kevin Lin <kevin.lin.p@gmail.com>
2025-03-23 00:22:33 -0700

    chat competions: start

--- src/routes/[threadId]/Thread.svelte
+++ src/routes/[threadId]/Thread.svelte
@@ -7,6 +7,7 @@
 	import ThreadTitle from './ThreadTitle.svelte';
 	import ThreadMessageInput from './ThreadMessageInput.svelte';
 	import DeleteButton from './DeleteButton.svelte';
+	import { debug } from 'openai/core';
 
 	let { threadId } = $props();
 	let thread = $state({});
@@ -19,6 +20,8 @@
 		}).unsubscribe;
 	});
 	onDestroy(() => unsubscribe && unsubscribe());
+
+	$inspect(thread);
 </script>
 
 {#if thread}
@@ -30,15 +33,23 @@
 		<DeleteButton {thread} />
 	</div>
 
-	{#if thread.messages?.length}
+	{#if thread.chatCompletions?.length}
 		<div class="pe-5">
-			{#each thread.messages as message, i (i)}
-				<div class="mb-3">
-					<p>{message.role}</p>
-					<pre class="whitespace-pre-wrap font-sans">{message.content}</pre>
-				</div>
+			{#each thread.chatCompletions as completion }
+				<pre class="whitespace-pre-wrap font-sans">{completion.choices[0].text.content}</pre>
 			{/each}
 		</div>
+	{:else}
+		{#if thread?.messages?.length}
+			<div class="pe-5">
+				{#each thread.messages as message, i (i)}
+					<div class="mb-3">
+						<p>{message.role}</p>
+						<pre class="whitespace-pre-wrap font-sans {message.role === 'user' ? 'text-gray-400' : ''}">{message.content}</pre>
+					</div>
+				{/each}
+			</div>
+		{/if}
 	{/if}
 
 	<ThreadMessageInput {thread} />
