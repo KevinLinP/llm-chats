af655e5d3dc4271c05b9e762dbc51a4b0bc60ffa
Kevin Lin <github@kevinlinp.org>
2025-12-23 18:38:59 -0500

    v3: move v2 files

---
+++ previous-versions/v2/.svelte-kit/generated/client/nodes/0.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/ui/routes/+layout.svelte";
---
+++ previous-versions/v2/.svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/ui/routes/settings/+page.svelte";
